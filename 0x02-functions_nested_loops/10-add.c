76549ab46bb2ba273e92cf1cc7a37c0f3fae91c8
galileofigaro <mbryant505@gmail.com>
2019-09-24 17:58:08 +0000

    Add file to add two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * add - adds two integers and returns sum
+ *@one: first number
+ *@two: second number
+ * Return: sum
+ *
+ */
+
+int add(int one, int two)
+{
+	int sum;
+
+	sum = one + two;
+	return (sum);
+}
