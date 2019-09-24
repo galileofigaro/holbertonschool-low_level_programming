a31281e6c26de37560637b90ce8e344d979928f6
galileofigaro <mbryant505@gmail.com>
2019-09-24 17:11:48 +0000

    Add seemingly working file, 24

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -18,7 +18,7 @@ void jack_bauer(void)
 		{
 			_putchar(hours / 10 + '0');
 			_putchar(hours % 10 + '0');
-			_putchar('\n');
+			_putchar(':');
 			_putchar(mins / 10 + '0');
 			_putchar(mins % 10 + '0');
 			_putchar('\n');
