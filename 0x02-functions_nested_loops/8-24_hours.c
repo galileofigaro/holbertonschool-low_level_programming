7cd1ad66fece24bc7077d331e29cd657ad7cf1cf
galileofigaro <mbryant505@gmail.com>
2019-09-24 17:03:47 +0000

    First attempt at 24

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * jack_bauer - Prints every minute of the day
+ *
+ * Return: zero
+ */
+
+void jack_bauer(void)
+{
+	int hours, mins;
+
+	hours = 0;
+	while (hours < 24)
+	{
+		mins = 0;
+		while (mins < 60)
+		{
+			_putchar(hours / 10 + '0');
+			_putchar(hours % 10 + '0');
+			_putchar('\n');
+			_putchar(mins / 10 + '0');
+			_putchar(mins % 10 + '0');
+			_putchar('\n');
+			mins++;
+		}
+	}
+	return;
+}
