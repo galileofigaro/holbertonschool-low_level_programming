1a5c46c8a7f7c4f64b6227c43bdaadbe647f1002
galileofigaro <mbryant505@gmail.com>
2019-09-26 18:59:25 +0000

    Add file to print numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - prints 0-14 ten times
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+        int lines;
+        int count;
+
+        for (lines = 0; lines < 10; lines++)
+        {
+                for (count = 0; count < 15; count++)
+                {
+                        if (count >= 10)
+                        {
+                                _putchar((count / 10) + '0');
+                        }
+                        _putchar((count % 10) + '0');
+                }
+                _putchar('\n');
+        }
+}
