3322cf284d1a1343f88a55ef7a692fc4a9fa56f4
galileofigaro <mbryant505@gmail.com>
2019-09-26 14:02:24 +0000

    Add file to mulitply two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * mul - mulitply two integers
+ *@a: first integer
+ *@b: second integer
+ *
+ * Return: product
+ */
+
+int mul(int a, int b)
+{
+	int prod;
+
+	prod = a * b;
+
+	return (prod);
+}
