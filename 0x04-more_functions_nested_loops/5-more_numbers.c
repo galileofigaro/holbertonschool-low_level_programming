214c9636fd6866ed889231f1eaead26ff105a8cf
galileofigaro <mbryant505@gmail.com>
2019-09-26 19:01:33 +0000

    Removed extra spaces

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,19 +8,19 @@
 
 void more_numbers(void)
 {
-        int lines;
-        int count;
+	int lines;
+	int count;
 
-        for (lines = 0; lines < 10; lines++)
-        {
-                for (count = 0; count < 15; count++)
-                {
-                        if (count >= 10)
-                        {
-                                _putchar((count / 10) + '0');
-                        }
-                        _putchar((count % 10) + '0');
-                }
-                _putchar('\n');
-        }
+	for (lines = 0; lines < 10; lines++)
+	{
+		for (count = 0; count < 15; count++)
+		{
+			if (count >= 10)
+			{
+				_putchar((count / 10) + '0');
+			}
+			_putchar((count % 10) + '0');
+		}
+		_putchar('\n');
+	}
 }
