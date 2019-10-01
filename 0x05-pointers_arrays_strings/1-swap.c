7feab853fa95a152bdc343da860aefa5e6c70ac3
galileofigaro <mbryant505@gmail.com>
2019-10-01 13:27:40 +0000

    Add file to swap pointer values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * swap_int - swaps values of two integers
+ *@a: first value
+ *@b: second value
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+
+	*a == *b;
+	*b == *a;
+}
