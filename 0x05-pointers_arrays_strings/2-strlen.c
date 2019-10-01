a6fc7e7c147f7f077cb8deb5e8465e02c0a7e58a
galileofigaro <mbryant505@gmail.com>
2019-10-01 13:50:11 +0000

    Add file to return string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _strlen - returns string length
+ *@s: string
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int count;
+
+	while (*s++ != '\0')
+		count++;
+}
