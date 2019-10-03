dc8f5b20e4f5b051089a45d7cff41ecd49775579
galileofigaro <mbryant505@gmail.com>
2019-10-03 18:25:26 +0000

    Add file to concatenate two strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,31 +10,14 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	char *pEnd = dest + _strlen(dest);
 
-	while (*src != '\0' && n--)
-		*pEnd++ = *src++;
+	int i, j;
 
-	*pEnd = '\0';
+	for (i = 0; *(dest + i) != '\0'; i++)
+		;
+	for (j = 0; *(src + j) != '\0'; j++, i++)
+		*(dest + i) = *(src + j);
+	*(dest + i) = '\0';
 
 	return (dest);
-
-}
-
-
-
-/**
- * _strlen - returns string length
- *@s: string
- * Return: length
- */
-
-int _strlen(char *s)
-{
-	int count;
-
-	while (*s++ != '\0')
-		count++;
-
-	return (count);
 }
