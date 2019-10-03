22551b735f825fe221d0109aebc82c01d8204098
galileofigaro <mbryant505@gmail.com>
2019-10-03 17:13:10 +0000

    Add file to concatenate two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,16 +9,14 @@
 
 char *_strcat(char *dest, char *src)
 {
-	while (*dest)
-		dest++;
+	int count, count2;
 
-	while (*src)
-	{
-		*dest = *src;
-		src++;
-		dest++;
-	}
-	*dest = '\0';
+	for (count = 0; *(dest + count) != '\0'; count++);
+
+	for (count2 = 0; *(src + count2) != '\0'; count2++, count++)
+		*(dest + count) = *(src + count2);
+	*(dest + count) = '\0';
 
 	return (dest);
+
 }
