b4847c1ed776d2a241708ab2f7c117a5606dd9b4
galileofigaro <mbryant505@gmail.com>
2019-10-03 17:15:02 +0000

    Add error fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,7 +11,8 @@ char *_strcat(char *dest, char *src)
 {
 	int count, count2;
 
-	for (count = 0; *(dest + count) != '\0'; count++);
+	for (count = 0; *(dest + count) != '\0'; count++)
+		;
 
 	for (count2 = 0; *(src + count2) != '\0'; count2++, count++)
 		*(dest + count) = *(src + count2);
