9906c705f5797606edea50e0f26fc438d3a9fa49
galileofigaro <mbryant505@gmail.com>
2019-10-31 21:44:54 +0000

    ADD: Prints elements of singly linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,7 +3,7 @@
 /**
  * print_list - Print elements of list
  *@h: list being printed
- * Return - number of nodes
+ * Return: number of nodes
  */
 
 size_t print_list(const list_t *h)
@@ -12,7 +12,10 @@ size_t print_list(const list_t *h)
 
 	for (nodeCount = 0; h != NULL; nodeCount++)
 	{
+		if (str == NULL)
+		{
 		printf("[%d] %s\n", h->len, h->str);
+		}
 		h = h->next;
 	}
 
