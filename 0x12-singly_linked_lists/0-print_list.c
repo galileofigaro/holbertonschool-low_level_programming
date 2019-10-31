cdb706ac541b4c824dae052cdbf20cb111ca6780
galileofigaro <mbryant505@gmail.com>
2019-10-31 21:42:06 +0000

    Add: Prints elements of singly linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_list - Print elements of list
+ *@h: list being printed
+ * Return - number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	int nodeCount;
+
+	for (nodeCount = 0; h != NULL; nodeCount++)
+	{
+		printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+	}
+
+	return (nodeCount);
+
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+Creating and using singly linked lists in the C language.
