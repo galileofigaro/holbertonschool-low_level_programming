82ef0fcd65b6c18222d41468656f7d47a26aa311
galileofigaro <mbryant505@gmail.com>
2019-11-01 03:35:38 +0000

    ADD: Function to free linked list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a linked list
+ *@head: pointer to first node
+ * Return: void
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	if (!head)
+		return;
+
+	temp = head;
+
+	while (temp != NULL)
+	{
+		free(temp->str);
+		free(temp);
+		temp = temp->next;
+	}
+
+
+}
